ab02d7efa60cbc4d1e184a01337369a4a529a8fa
playatanu <playatanu@gmail.com>
2025-05-22 09:06:02 +0530

    capture fswebcam

--- core/capture.py
+++ core/capture.py
@@ -1,10 +1,11 @@
+import os
 import cv2
+import subprocess
 from core.config import config
 
 CAMERA_ID = config["camera"]["id"]
 WIDTH = config["camera"]["width"]
 HEIGHT = config["camera"]["height"]
-CROP = config["camera"]["crop"]
 OUTPUT_FILENAME = config["camera"]["output_filename"]
 
 
@@ -13,34 +14,52 @@ def store_image(image):
 
 
 def capture():
-    cap = cv2.VideoCapture(CAMERA_ID)
-
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
-
-    if not cap.isOpened():
-        print("Error: Cannot open camera")
-        exit()
-
-    ret, frame = cap.read()
-
-    if ret:
-        height, width = frame.shape[:2]
-
-        crop_size = min(height, width, CROP)
-
-        start_x = (width - crop_size) // 2
-        start_y = (height - crop_size) // 2
-
-        cropped_image = frame[
-            start_y : start_y + crop_size, start_x : start_x + crop_size
+    subprocess.run(
+        [
+            "fswebcam",
+            "-r",
+            f"{WIDTH}x{HEIGHT}",
+            "-d" f"/dev/video{CAMERA_ID}",
+            "--no-banner",
+            "image.jpg",
         ]
+    )
 
-        cap.release()
-        return cropped_image
+    cwd = os.getcwd()
+    image_path = os.path.join(cwd, "image.jpg")
+    image = cv2.imread(image_path)
+    return image
 
-    else:
-        print("Error: Failed to capture image.")
 
-    cap.release()
-    return frame
+# def capture():
+#     cap = cv2.VideoCapture(CAMERA_ID)
+
+#     cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
+#     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
+
+#     if not cap.isOpened():
+#         print("Error: Cannot open camera")
+#         exit()
+
+#     ret, frame = cap.read()
+
+#     if ret:
+#         height, width = frame.shape[:2]
+
+#         crop_size = min(height, width, CROP)
+
+#         start_x = (width - crop_size) // 2
+#         start_y = (height - crop_size) // 2
+
+#         cropped_image = frame[
+#             start_y : start_y + crop_size, start_x : start_x + crop_size
+#         ]
+
+#         cap.release()
+#         return cropped_image
+
+#     else:
+#         print("Error: Failed to capture image.")
+
+#     cap.release()
+#     return frame
